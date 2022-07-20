7c033babb76122980569df446ff96aa07c933c1a
TaylorBruun <taylorbruun@gmail.com>
2022-07-20 14:27:43 -0600

    init

---
+++ CSGregs-auth/Models/Account.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace CSGregs-auth.Models
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Picture { get; set; }
+    }
+}
